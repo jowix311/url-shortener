02fd4294de886c249312f088c3d41a4e6161e83a
jowix311 <jowix311@gmail.com>
2025-05-21 14:34:45 +0800

    Update typo on class name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="font-bold text-red5100">
+      <main className="font-bold text-blue-300">
         Under construction... <Button>Click me</Button>
       </main>
     </div>
