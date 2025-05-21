5f5ff65d6c1a8203a788296bdd8bc9d1c3a0ff27
jowix311 <jowix311@gmail.com>
2025-05-21 14:29:52 +0800

    VS Code setting and prettier class auto sort setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="font-bold text-red5100">
         Under construction... <Button>Click me</Button>
       </main>
     </div>
